c1df39dfe59b23b4585a3457b6fa79bee44e5fb9
Anh Minh <AnhMinh@Anhs-MacBook-Pro.local>
2013-03-09 15:51:19 +0700

    Initial Commit

---
+++ BookManager/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BookManager
+//
+//  Created by Anh Minh on 3/9/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BookManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BookManager
+//
+//  Created by Anh Minh on 3/9/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
